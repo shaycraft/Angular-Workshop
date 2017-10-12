53c156ba598296f71d3a0ff2d7f0f079bed5298f
Sam Haycraft <shaycraft@metlife.com>
2017-10-12 12:16:34 -0600

    lazy loaded module (without guard)

---
+++ src/app/collection/collection.module.ts
@@ -0,0 +1,49 @@
+import { NgModule } from '@angular/core';
+import { CollectionComponent } from './collection.component';
+import { RatingCategoryPipe } from '../pipes/rating-category.pipe';
+import { RatingComponent } from '../rating/rating.component';
+import { BookDetailComponent } from '../book-detail/book-detail.component';
+import { NewBookComponent } from '../new-book/new-book.component';
+import { BookGuardService } from '../guards/book-guard.service';
+import { DataService } from '../services/data.service';
+import { CollectionRoutingModule } from './collection.routing.module';
+import { MatListModule, MatTabsModule, MatSnackBarModule, MatDialogModule, MatCardModule, MatIconModule, MatSlideToggleModule, MatButtonModule, MatLineModule, MatInputModule, MatToolbarModule } from '@angular/material';
+
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+import { HttpModule }from '@angular/http';
+
+
+@NgModule({
+    imports: [
+        CommonModule,
+        FormsModule,
+        HttpModule,
+        CollectionRoutingModule,
+        MatListModule,
+        MatTabsModule,
+        MatSnackBarModule,
+        MatDialogModule,
+        MatCardModule,
+        MatIconModule,
+        MatSlideToggleModule,
+        MatButtonModule,
+        MatLineModule,
+        MatInputModule,
+        MatToolbarModule,
+    ],
+    entryComponents: [
+        NewBookComponent
+    ],
+    declarations: [
+        CollectionComponent,
+        RatingComponent,
+        BookDetailComponent,
+        NewBookComponent,
+        RatingCategoryPipe
+    ],
+    providers: [
+        //BookGuardService, 
+        DataService]
+})
+export class CollectionModule { }
